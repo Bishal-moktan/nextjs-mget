0a09571cd4736a1aeb55a2774400de51983813f9
Bishal Moktan <busial.mok9@gmail.com>
2023-04-18 16:31:42 +0530

    Change in styling in the feature section of the home page

--- components/CardList/CardList.js
+++ components/CardList/CardList.js
@@ -1,21 +1,36 @@
 import Image from 'next/image';
 import styles from './List.module.css';
 const CardList = ({ content, title }) => {
+  const firstRow = content.slice(0, 3);
+  const secondRow = content.slice(3, content.length);
   return (
-    <article className="container">
+    <article className={`container ${styles.feature}`}>
       <p className="margin">{title}</p>
-      <div className={styles.cards}>
-        {content.map((item, index) => {
+      <div className={styles.firstRow}>
+        {firstRow.map((item, index) => {
           return (
             <div className={styles.card} key={index}>
-              <header className={styles.header}>
-                <h3>{item.title}</h3>
-              </header>
-              <div className={styles.left}>
-                <div className={styles.icon}>
-                  <Image src={item.icon} height={'auto'} width={'auto'} />
-                </div>
+              <div className={styles.icon}>
+                <Image src={item.icon} height={'auto'} width={'auto'} />
               </div>
+              <header className={styles.header}>
+                <h3>{item.title} </h3>
+              </header>
+              <div className={styles.content}>{item.info}</div>
+            </div>
+          );
+        })}
+      </div>
+      <div className={styles.secondRow}>
+        {secondRow.map((item, index) => {
+          return (
+            <div className={styles.card} key={index}>
+              <div className={styles.icon}>
+                <Image src={item.icon} height={'auto'} width={'auto'} />
+              </div>
+              <header className={styles.header}>
+                <h3>{item.title} </h3>
+              </header>
               <div className={styles.content}>{item.info}</div>
             </div>
           );
